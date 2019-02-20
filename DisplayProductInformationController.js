87cd33674f2e0e5ed01c11a95a0e9918bb524dc5
kumarmbee255 <47459305+kumarmbee255@users.noreply.github.com>
2019-02-20 21:54:56 +0530

    aura

---
+++ DisplayProductInformationController.js
@@ -0,0 +1,23 @@
+({
+	doInit : function(component, event, helper) {
+		var caseId = component.get("v.recordId");//get the case record id from context
+        var getProductInfo = component.get("c.getAssociatedProductInfo");//invoke method to get the product information associated to case's contact record.
+        getProductInfo.setParams({
+            caseId : component.get("v.recordId")
+        });
+        
+        getProductInfo.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var productInfo = response.getReturnValue();
+                component.set("v.productInformation", response.getReturnValue());
+            } 
+            else{
+                component.set("v.componentErrors",'No product information found.'); //display info to user in case of no product associated/found.
+            }
+        });        
+         
+        
+        $A.enqueueAction(getProductInfo); 
+	}
+})
